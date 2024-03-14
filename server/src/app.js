13c1d0e31f7fccbe55b8252d820a1f1fdab563de
Rukas <45427438+Rukas90@users.noreply.github.com>
2024-03-14 17:20:08 +0200

    Update README

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,8 @@
 import dotenv      from 'dotenv'
 import { Server }  from './server.js'
-import { mail } from './mailer.js'
 
 dotenv.config()
 
 const server = new Server(process.env.SERVER_PORT || 3000)
 
 server.start()
-
-/*mail({
-    recipient: 'watchfinddo@gmail.com',
-    subject: 'Test Subject',
-    body: 'Test Body...'
-})*/
