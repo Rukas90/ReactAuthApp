2d7c78c085f2097c4a651f156f0591eb7985e7d2
Rukas Skirkevicius <rukas.skirkevicius@gmail.com>
2026-02-02 00:13:27 +0200

    Stop tracking ignored files
    Cleared git cache and stopped tracking node_modules and shared dist output folder.

--- shared/dist/lib/AuthUser.d.ts
+++
@@ -1,9 +0,0 @@
-export type SessionData = {
-    user: AuthUser | null;
-    canRefresh: boolean;
-};
-export type Scope = "mfa:verify" | "admin:access";
-export type AuthUser = {
-    scope: Scope[];
-    expiresAt: number;
-};
